a6b368f3ecd77ce7a5c4bedd85e8050601fd4e27
Aleksandar Ristic <aleksandar1983@gmail.com>
2025-04-13 20:46:21 +0200

    Fixes

--- app/utils.py
+++ app/utils.py
@@ -1,9 +1,12 @@
+import json
+
 from fastapi import Request
 from fastapi.templating import Jinja2Templates
 from starlette import status
 from starlette.responses import RedirectResponse, Response
 
 templates = Jinja2Templates(directory="app/templates")
+templates.env.filters["escapejs"] = lambda v: json.dumps(str(v))[1:-1]
 
 
 def render_template(template_name: str, request: Request, context: dict) -> Response:
@@ -14,7 +17,9 @@ def render_template(template_name: str, request: Request, context: dict) -> Resp
     return templates.TemplateResponse(template_name, context)
 
 
-def redirect_back(request: Request, fallback_url: str = "/", status_code: int = status.HTTP_302_FOUND) -> RedirectResponse:
+def redirect_back(
+    request: Request, fallback_url: str = "/", status_code: int = status.HTTP_302_FOUND
+) -> RedirectResponse:
     ref = request.headers.get("referer")
     return RedirectResponse(ref or fallback_url, status_code=status.HTTP_302_FOUND)
 
